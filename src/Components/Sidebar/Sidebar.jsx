678fef5bf28581e26d556549b667b3253a5f809a
LeTThuyHien <22708291.hien@student.iuh.edu.vn>
2025-04-04 03:03:51 +0700

    Them logo

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,14 @@
+import img from "../../assets/img/logo.png";
 import "./Sidebar.css";
 function Sidebar() {
-    return ( 
-        <aside className="sidebar"></aside>
-     );
+    return (
+        <aside className="sidebar">
+            <div className="img-logo">
+                <img src={img} alt="logo" />
+            </div>
+        </aside>
+
+    );
 }
 
 export default Sidebar;
