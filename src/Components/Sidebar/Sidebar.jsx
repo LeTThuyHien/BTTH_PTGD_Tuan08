49c3c354f61d8f1f4df7a9e6ff8bed660b0fc500
LeTThuyHien <22708291.hien@student.iuh.edu.vn>
2025-04-06 00:01:11 +0700

    7. isActive (NavLink...) + minhchung

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,26 @@
 import img from "../../assets/img/logo.png";
 import "./Sidebar.css";
+import Menu from "./Menu/Menu";
+import MenuItem from "./Menu/MenuItem";
+import { FaChartPie, FaCode, FaFolder, FaLayerGroup } from "react-icons/fa";
+import { FaMessage, FaPeopleGroup } from "react-icons/fa6";
 function Sidebar() {
     return (
         <aside className="sidebar">
             <div className="img-logo">
                 <img src={img} alt="logo" />
             </div>
+            <Menu>
+                <MenuItem title= "Dashboard" to="/" icon = {<FaLayerGroup />} />
+                <MenuItem title= "Projects" to="/projects" icon={<FaFolder/>} />
+                <MenuItem title= "Teams" to="/teams" icon={<FaPeopleGroup/>} />
+                <MenuItem title= "Analytics" to="/analytics" icon={<FaChartPie/>} />
+                <MenuItem title= "Messages" to="/messages" icon={<FaMessage/>} />
+                <MenuItem title= "Integration" to="/integration" icon={<FaCode/>} />
+            </Menu>
+
+
+
         </aside>
 
     );
